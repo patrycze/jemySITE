f029c73440c24fe154cdffb17ed86e04288a98f7
patrycze <patryk.pazura@gmail.com>
2017-06-11 14:34:09 +0200

    registration

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+
+$con=mysqli_connect("localhost","pazura","baran","testgastro");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+$query = "INSERT INTO testgastro.klient (Imie, Nazwisko, Email, Telefon) VALUES ('{$name}','{$surname}','{$email}','{$tel}')";
+
+echo $query;
+mysqli_query($con,$query);
+?>
